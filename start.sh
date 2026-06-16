d4dc2e08b8a0e8dd330bbb05e130809453ca187a
Soudeepta146 <soudeepta2004bhattacharjee@gmail.com>
2026-06-17 00:52:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-listmonk --install --yes || true
+cat > /tmp/config.toml << EOF
+[app]
+address = "0.0.0.0:9000"
+
+[db]
+host = "${LISTMONK_db_host}"
+port = ${LISTMONK_db_port}
+user = "${LISTMONK_db_user}"
+password = "${LISTMONK_db_password}"
+database = "${LISTMONK_db_database}"
+ssl_mode = "disable"
+max_open = 25
+max_idle = 25
+max_lifetime = "300s"
+EOF
+
+listmonk --config /tmp/config.toml
